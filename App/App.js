932044b0c46d1848069b77c3b9b6c39315a3b900
Tim <34155548+TotallyTheTim@users.noreply.github.com>
2020-02-02 19:44:25 +0100

    getting started on the server

--- App/App.js
+++ App/App.js
@@ -1,10 +1,11 @@
-import React, { Component } from 'react';
-import { Text, View, ImageBackground, Button, TouchableOpacity, StatusBar } from 'react-native';
+import React, {Component} from 'react';
+import {Text, View, ImageBackground, Button, TouchableOpacity, StatusBar} from 'react-native';
 import Home from './components/Home'
 import Drinks from './components/Drinks'
 import Extras from './components/Extras'
 import Pizza from './components/Pizza'
 import {styles} from './AppStyle';
+import io from "socket.io-client";
 
 export default class DaanimosApp extends Component {
 
@@ -16,8 +17,9 @@ export default class DaanimosApp extends Component {
         this.getRoute = this.getRoute.bind(this);
         this.setRoute = this.setRoute.bind(this);
     }
-    getRoute(){
-        switch(this.state.route) {
+
+    getRoute() {
+        switch (this.state.route) {
             case 'home':
                 return <Home setRoute={this.setRoute}/>;
             case 'drinks':
@@ -27,10 +29,11 @@ export default class DaanimosApp extends Component {
             case 'pizza':
                 return <Pizza/>;
             default:
-            return ''
+                return ''
         }
     }
-    setRoute(routeName){
+
+    setRoute(routeName) {
         this.setState({
             route: routeName,
         });
@@ -38,7 +41,14 @@ export default class DaanimosApp extends Component {
 
     componentDidMount() {
         StatusBar.setHidden(true);
-     }
+
+        this.socket = io('http://192.168.56.1:3000/');
+        this.socket.on("connect", () => {
+
+            console.log('NICE');
+            this.setRoute("pizza");
+        });
+    }
 
     render() {
         return (
@@ -46,13 +56,15 @@ export default class DaanimosApp extends Component {
                 <View style={{flexDirection: 'row', justifyContent: 'center'}}>
                     {/* <Button style={styles.button} title="test" onPress={()=>{this.setRoute("test")}}>test</Button>
                     <Button style={styles.button} title="home" onPress={()=>{this.setRoute("home")}}>home</Button> */}
-                    <TouchableOpacity onPress={()=>{this.setRoute("home")}}><Text style={styles.button}>Home</Text></TouchableOpacity>
+                    <TouchableOpacity onPress={() => {
+                        this.setRoute("home")
+                    }}><Text style={styles.button}>Home</Text></TouchableOpacity>
                 </View>
                 <ImageBackground source={require("../App/assets/home.jpg")} style={styles.screen}>
                     <View style={styles.main}>
                         {this.getRoute()}
                     </View>
-            </ImageBackground>
+                </ImageBackground>
             </View>
         );
     }
---
+++ Socket/server.js
@@ -0,0 +1,28 @@
+// Require HTTP module (to start server) and Socket.IO
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+
+server.listen(3000);
+
+// Create a Socket.IO instance, passing it our server
+var socket = io.listen(server);
+
+// Add a connect listener
+socket.on('connection', function (client) {
+    console.log("CONNECT:",client.id);
+    // Success!  Now listen to messages to be received
+    client.on('message', function (event) {
+        console.log('Received message from client!', event);
+    });
+    client.on('disconnect', function () {
+        // clearInterval(interval);
+        console.log("DISCONNENT",client.id);
+    });
+
+});
+
+app.get('/', function(req, res,next) {
+    res.sendFile(__dirname + '/index.html');
+});
