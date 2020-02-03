ab96f5cb6bc05b52d3e9fdc57fd59963814e7c38
Daan <daank2000@hotmail.nl>
2020-02-03 20:58:57 +0100

    test commit

--- App/AppStyle.js
+++ App/AppStyle.js
@@ -7,7 +7,7 @@ const styles = StyleSheet.create({
         backgroundColor: 'rgba(0,0,0,.2)'
     },
     red: {
-        color: '#f00',
+        color: '#ff0',
     },
     main: {
         flex: 1,
