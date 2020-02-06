8681ec5dddf627d361890ac8bc3a40678224ae3a
tim <tim@noprotocol.nl>
2020-02-06 12:30:04 +0100

    finally got the QR code to work

--- App/components/Connect.js
+++ App/components/Connect.js
@@ -1,44 +1,54 @@
 import React, {Component} from 'react';
 import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 import {styles} from '../AppStyle';
-import * as Permissions from 'expo-permissions';
-import {RNCamera} from 'react-native-camera';
+import {BarCodeScanner} from 'expo-barcode-scanner';
 
 class Connect extends Component {
     constructor(props) {
         super(props);
-        this.state = {cameraPermission: false, uri: 'http://192.168.178.129:3000/'};
+        this.handleBarCodeScanned = this.handleBarCodeScanned.bind(this);
+        this.state = {cameraPermission: false, scanned: false, uri: ''};
+    }
+
+    async useEffect() {
+        const {status} = await BarCodeScanner.requestPermissionsAsync().catch(
+            (e) => {
+                console.log("BarcodeSnanner request permission error: ", e)
+            }
+        );
+        this.setState({cameraPermission: status === ' granted'})
     }
 
     componentDidMount() {
-        setInterval(getCameraPermision, 10000)
-        this.setState({cameraPermission: getCameraPermision()})
+        let _this = this;
+        this.useEffect();
     }
+
+    handleBarCodeScanned(data) {
+        if (!this.state.scanned) {
+            this.setState({scanned: true,});
+            this.setState({uri: data.data});
+        }
+    };
 
 
     render() {
         return (
             <View style={styles.container}>
-                {this.state.cameraPermission === true ? <RNCamera
-                    ref={ref => {
-                        this.camera = ref;
-                    }}
-                    style={{
-                        flex: 1,
-                        width: '100%',
-                    }}
-                >
-                </RNCamera> : null}
+                <BarCodeScanner
+                    style={{flex: 1, justifyContent: 'flex-end', alignItems: 'center', backgroundColor: '#000'}}
+                    onBarCodeScanned={(data) => {
+                        this.handleBarCodeScanned(data)
+                    }}/>
                 <Text>{this.state.uri}</Text>
-                <TouchableOpacity onPress={() => {
-                    this.props.makeSocket('http://192.168.178.129:3000/')
-                }}><Text style={styles.button}>Connect</Text></TouchableOpacity>
+                {this.state.scanned ? <TouchableOpacity onPress={() => { this.setState({uri: '', scanned: false})}}><Text style={styles.button}>Scan Again</Text></TouchableOpacity> : <Text>No scan yet</Text>}
+                <TouchableOpacity onPress={() => { this.props.makeSocket(this.state.uri) }}><Text style={styles.button}>Connect</Text></TouchableOpacity>
             </View>
         );
     }
 }
 
-async function getCameraPermision(){
+async function getCameraPermision() {
     // permissions returns only for location permissions on iOS and under certain conditions, see Permissions.LOCATION
     let {status, permissions} = await Permissions.askAsync(Permissions.CAMERA);
     if (status === 'granted') {
